0725f9fe2342ad5dac81dd139030e977428d79bd
defnot001 <marc.miller994@gmail.com>
2024-02-07 00:15:41 +0100

    change method to delete

--- microservice/src/routes/admin.rs
+++ microservice/src/routes/admin.rs
@@ -1,7 +1,6 @@
 use crate::{handlers::admin, AppState};
 use axum::{
-    routing::get,
-    routing::{patch, post},
+    routing::{delete, get, patch, post},
     Router,
 };
 
@@ -9,7 +8,7 @@ pub fn admin_router(app_state: AppState) -> Router {
     Router::new()
         .route("/list", get(admin::handle_admin_list))
         .route("/add", post(admin::handle_admin_add))
-        .route("/delete", post(admin::handle_admin_delete))
+        .route("/delete", delete(admin::handle_admin_delete))
         .route("/update", patch(admin::handle_admin_update))
         .with_state(app_state)
 }
