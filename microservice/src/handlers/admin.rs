9ceed6cfc31886ddcd314fdd091bfc10ecf2176e
defnot001 <marc.miller994@gmail.com>
2024-02-07 00:16:02 +0100

    implemented the 4 method handlers

--- microservice/src/handlers/admin.rs
+++ microservice/src/handlers/admin.rs
@@ -1,14 +1,33 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use axum_extra::TypedHeader;
 use headers::{authorization::Bearer, Authorization};
-use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
-use sqlx::{pool::PoolConnection, Sqlite, SqliteConnection, SqlitePool};
+use serde::Serialize;
+use serde_json::json;
+use sqlx::{pool::PoolConnection, Sqlite, SqlitePool};
 
-use crate::{config::Config, AppState};
+use crate::{
+    config::Config,
+    database::{AdminDeleteBody, AdminPostBody, AdminUpdateBody, DatabaseHelper},
+    AppState,
+};
 
-pub async fn handle_admin_list() -> impl IntoResponse {
-    "Admin page"
+pub async fn handle_admin_list(
+    TypedHeader(auth): TypedHeader<Authorization<Bearer>>,
+    State(app_state): State<AppState>,
+) -> impl IntoResponse {
+    if !check_auth(auth, app_state.config.clone()).await {
+        return StatusCode::UNAUTHORIZED.into_response();
+    }
+
+    let Ok(mut connection) = acquire_connection(&app_state.db_pool).await else {
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    };
+
+    if let Ok(users) = DatabaseHelper::list_users(&mut connection).await {
+        return Json(users).into_response();
+    }
+
+    StatusCode::INTERNAL_SERVER_ERROR.into_response()
 }
 
 pub async fn handle_admin_add(
@@ -24,19 +43,88 @@ pub async fn handle_admin_add(
         return StatusCode::INTERNAL_SERVER_ERROR.into_response();
     };
 
-    if add_user(body, &mut connection).await.is_ok() {
-        return Json(AdminPostResponse { success: true }).into_response();
+    match DatabaseHelper::add_user(body, &mut connection).await {
+        Ok(_) => {
+            return Json(AdminResponseBody {
+                success: true,
+                reason: None,
+            })
+            .into_response();
+        }
+        Err(e) => {
+            let status_code = StatusCode::CONFLICT;
+            let body = Json(AdminResponseBody {
+                success: false,
+                reason: Some(e.to_string()),
+            });
+
+            return (status_code, body).into_response();
+        }
+    }
+}
+
+pub async fn handle_admin_delete(
+    TypedHeader(auth): TypedHeader<Authorization<Bearer>>,
+    State(app_state): State<AppState>,
+    Json(body): Json<AdminDeleteBody>,
+) -> impl IntoResponse {
+    if !check_auth(auth, app_state.config.clone()).await {
+        return StatusCode::UNAUTHORIZED.into_response();
     }
 
-    StatusCode::INTERNAL_SERVER_ERROR.into_response()
+    let Ok(mut connection) = acquire_connection(&app_state.db_pool).await else {
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    };
+
+    match DatabaseHelper::delete_user(body, &mut connection).await {
+        Ok(_) => {
+            return Json(AdminResponseBody {
+                success: true,
+                reason: None,
+            })
+            .into_response();
+        }
+        Err(e) => {
+            return Json(AdminResponseBody {
+                success: false,
+                reason: Some(e.to_string()),
+            })
+            .into_response();
+        }
+    }
 }
 
-pub async fn handle_admin_delete() -> impl IntoResponse {
-    "Admin delete"
-}
+pub async fn handle_admin_update(
+    TypedHeader(auth): TypedHeader<Authorization<Bearer>>,
+    State(app_state): State<AppState>,
+    Json(body): Json<AdminUpdateBody>,
+) -> impl IntoResponse {
+    if !check_auth(auth, app_state.config.clone()).await {
+        return StatusCode::UNAUTHORIZED.into_response();
+    }
 
-pub async fn handle_admin_update() -> impl IntoResponse {
-    "Admin update"
+    let Ok(mut connection) = acquire_connection(&app_state.db_pool).await else {
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    };
+
+    match DatabaseHelper::update_user(body, &mut connection).await {
+        Ok(user) => {
+            let response = json!({
+                "success": true,
+                "reason": null,
+                "updated_user": user,
+            });
+
+            return Json(response).into_response();
+        }
+        Err(e) => {
+            return Json(AdminResponseBody {
+                success: false,
+                reason: Some(e.to_string()),
+            })
+            .into_response();
+        }
+    }
 }
 
 async fn acquire_connection(db_pool: &SqlitePool) -> anyhow::Result<PoolConnection<Sqlite>> {
@@ -47,40 +135,11 @@ async fn acquire_connection(db_pool: &SqlitePool) -> anyhow::Result<PoolConnecti
 }
 
 async fn check_auth(auth: Authorization<Bearer>, config: Config) -> bool {
-    println!("Auth: {:?} | Config: {:?}", auth, config.ADMIN_TOKEN);
-    let config_admin_token = config.ADMIN_TOKEN;
-
-    auth.0.token() == config_admin_token
-}
-
-async fn add_user(data: AdminPostBody, connection: &mut SqliteConnection) -> anyhow::Result<()> {
-    let hashed_password = hash_password(&data.server_id);
-
-    sqlx::query("INSERT INTO users (server_id, server_list, auth_token) VALUES (?, ?, ?)")
-        .bind(&data.server_id)
-        .bind(serde_json::to_string(&data.server_list)?)
-        .bind(hashed_password)
-        .execute(connection)
-        .await?;
-
-    Ok(())
-}
-
-fn hash_password(password: &str) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(password);
-    hasher.update(b"lkajsdf982");
-    format!("{:x}", hasher.finalize())
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AdminPostBody {
-    pub server_id: String,
-    pub server_list: Vec<String>,
-    pub auth_token: String,
+    auth.0.token() == config.ADMIN_TOKEN
 }
 
 #[derive(Debug, Serialize)]
-struct AdminPostResponse {
+struct AdminResponseBody {
     success: bool,
+    reason: Option<String>,
 }
