4705689ab12ff1a42ba8fdef825caeae8dea5833
defnot001 <marc.miller994@gmail.com>
2024-02-08 10:52:15 +0100

    simplify admin auth since we don't need the user passed trough

--- microservice/src/middleware/mw_auth_admin.rs
+++ microservice/src/middleware/mw_auth_admin.rs
@@ -9,29 +9,24 @@ use headers::{authorization::Bearer, Authorization};
 
 use crate::{config::Config, AppState};
 
-struct AdminUser {
-    authorized: bool,
-}
-
 pub async fn admin_auth(
     State(app_state): State<AppState>,
     TypedHeader(auth): TypedHeader<Authorization<Bearer>>,
     req: Request,
     next: Next,
 ) -> Result<Response, StatusCode> {
-    if let Some(user) = authorize_user(&app_state.config, &auth).await {
-        if user.authorized {
-            return Ok(next.run(req).await);
-        }
+    if authorize_user(&app_state.config, &auth).is_ok() {
+        return Ok(next.run(req).await);
     }
 
     Err(StatusCode::UNAUTHORIZED)
 }
 
-async fn authorize_user(config: &Config, auth_token: &Authorization<Bearer>) -> Option<AdminUser> {
-    if auth_token.0.token() != config.ADMIN_TOKEN {
-        return None;
+fn authorize_user(config: &Config, auth_token: &Authorization<Bearer>) -> anyhow::Result<()> {
+    if auth_token.0.token() == config.ADMIN_TOKEN {
+        return Ok(());
     }
 
-    Some(AdminUser { authorized: true })
+    tracing::error!("Unauthorized access attempt");
+    Err(anyhow::anyhow!("Unauthorized"))
 }
