ff19b36ebdef564263fdeba7f01033317d330fa2
defnot001 <marc.miller994@gmail.com>
2024-02-10 10:41:44 +0100

    merge create tables into one file

--- microservice/database/migrations/20240205_create_tables.sql
+++ microservice/database/migrations/20240205_create_tables.sql
@@ -1,5 +1,12 @@
-CREATE TABLE IF NOT EXISTS users (
-  server_id TEXT NOT NULL UNIQUE PRIMARY KEY,
-  server_list TEXT NOT NULL,
-  auth_token TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS users
+(
+    server_id   TEXT NOT NULL UNIQUE PRIMARY KEY,
+    server_list TEXT NOT NULL,
+    auth_token  TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS configs
+(
+    client_id     TEXT NOT NULL UNIQUE PRIMARY KEY,
+    subscriptions TEXT NOT NULL
 );
