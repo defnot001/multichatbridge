efdf6ea0d3972775c24539387e4f6f1271d55eb4
defnot001 <marc.miller994@gmail.com>
2024-02-12 17:55:34 +0100

    remove websocket logic

--- microservice/src/routes/websocket.rs
+++ microservice/src/routes/websocket.rs
@@ -1,26 +1,23 @@
-use std::{net::SocketAddr, ops::ControlFlow};
+use std::net::SocketAddr;
 
 use axum::routing::post;
 use axum::{
-    extract::{
-        ws::{Message, WebSocket},
-        ConnectInfo, WebSocketUpgrade,
-    },
+    extract::{ConnectInfo, WebSocketUpgrade},
     response::IntoResponse,
     Router,
 };
-use futures_util::{
-    stream::{SplitSink, SplitStream},
-    SinkExt, StreamExt,
-};
 use sqlx::SqlitePool;
-use tracing::{info, warn};
+use tracing::info;
 
-use crate::message::process_message;
-
+use crate::middleware::mw_auth_websocket::mw_websocket_auth;
+use crate::websocket::websocket_handler::handle_socket;
 pub fn websocket_routes(db_pool: SqlitePool) -> Router {
     Router::new()
         .route("/ws", post(handle_websocket))
+        .route_layer(axum::middleware::from_fn_with_state(
+            db_pool.clone(),
+            mw_websocket_auth,
+        ))
         .with_state(db_pool)
 }
 
@@ -31,93 +28,4 @@ pub async fn handle_websocket(
     info!("User agent at {addr} connected.");
 
     ws.on_upgrade(move |socket| handle_socket(socket, addr))
-}
-
-/// Websocket statemachine, one will be spawned per connection
-async fn handle_socket(socket: WebSocket, address: SocketAddr) {
-    let mut handler = WebSocketHandler::new(socket, address);
-
-    // ping the client, if it does not respond, drop the connection
-    if handler.ping_client().await.is_break() {
-        return;
-    }
-
-    // wait for a single message from the client (usually the pong), if it does not respond, drop the connection
-    if handler.receive_single_message().await.is_break() {
-        return;
-    }
-
-    tokio::spawn(async move {
-        while let Some(Ok(msg)) = handler.receiver.next().await {
-            if process_message(msg.clone(), address).is_break() {
-                break;
-            }
-
-            if handler.sender.send(msg).await.is_err() {
-                break;
-            }
-        }
-    });
-
-    info!("Websocket context {address} destroyed");
-}
-
-/// This struct holds the `websocket connection` and the `client's address` and provides
-/// convenience methods for sending and receiving messages.
-#[derive(Debug)]
-struct WebSocketHandler {
-    address: SocketAddr,
-    sender: SplitSink<WebSocket, Message>,
-    receiver: SplitStream<WebSocket>,
-}
-
-impl WebSocketHandler {
-    /// Creates a new `SplitWebsocketHandler` from a `WebSocket` and a `SocketAddr`.
-    fn new(socket: WebSocket, address: SocketAddr) -> Self {
-        let (sender, receiver) = socket.split();
-
-        Self {
-            address,
-            sender,
-            receiver,
-        }
-    }
-
-    async fn ping_client(&mut self) -> ControlFlow<(), ()> {
-        if self.sender.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
-            info!("Pinged {} successfully", self.address);
-
-            ControlFlow::Continue(())
-        } else {
-            warn!(
-                "Could not send ping to {}, closing connection...",
-                self.address
-            );
-
-            ControlFlow::Break(())
-        }
-    }
-
-    /// Waits for a single message from the client and returns `ControlFlow::Continue` if the
-    /// message was received successfully or `ControlFlow::Break` if the message could not be
-    /// received if there was an error or if the client closed the connection. Usually this would
-    /// be used after sending a ping to the client to check if the client is still alive. This will
-    /// block the current task, but will not block other client's connections.
-    async fn receive_single_message(&mut self) -> ControlFlow<(), ()> {
-        if let Some(msg) = self.receiver.next().await {
-            if let Ok(msg) = msg {
-                if process_message(msg, self.address).is_break() {
-                    return ControlFlow::Break(());
-                }
-
-                ControlFlow::Continue(())
-            } else {
-                warn!("client {} abruptly disconnected", self.address);
-                ControlFlow::Break(())
-            }
-        } else {
-            warn!("stream at {} has closed", self.address);
-            ControlFlow::Break(())
-        }
-    }
 }
